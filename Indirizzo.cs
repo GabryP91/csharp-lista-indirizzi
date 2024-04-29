f5383fc99b0d0f5d092bbddf671025bb247cd7d4
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-29 17:27:46 +0200

    Aggiungere i file di progetto.

---
+++ Indirizzo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        //attributi
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private int zip;
+
+        //metodi
+
+        //Costruttore
+        public Indirizzo(string name, string surname, string street, string city, string province, int zip)
+        {
+
+            Name = name;
+            Surname = surname;
+            City = city;
+            Street = street;
+            Province = province;
+            ZIP = zip;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Province
+        {
+            get { return province; }
+            set { province = value; }
+        }
+        public int ZIP
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        //modifico funzione per la riscrittura dei dati dalla mia lista al file csv
+        public override string ToString()
+        {
+            return $"{Name}, {Surname} , {Street} , {City}, {Province}, (CAP:{ZIP})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿/*
+ 
+Oggi esercitazione sui file, ossia vi chiedo di prendere dimestichezza con quanto appena visto sui file in classe, in particolare nel live-coding di oggi.
+In questo esercizio dovrete leggere un file CSV, che cambia poco da quanto appena visto nel live-coding in classe, e salvare tutti gli indirizzi in esso contenuti 
+all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+
+**Attenzione:** gli ultimi 3 indirizzi presentano dei possibili “casi particolari” che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire come meglio crediate queste casistiche.
+ 
+**Bonus**: iterare la lista di indirizzi e risalvarli in un file.
+
+*/
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\dati utente\\Desktop\\Progetti Boolean\\Esercizi\\CORSO.net\\csharp-lista-indirizzi\\addresses.csv"; // Percorso del file CSV
+            string filePath2 = "D:\\dati utente\\Desktop\\Progetti Boolean\\Esercizi\\CORSO.net\\csharp-lista-indirizzi\\addresses2.csv";
+
+            //istanzio la mia lista di indirizzi
+            List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+            using var stream = File.OpenText(filePath); //tramite uso parola chiave using chiamo automaticamente a fine esecuzione il metodo Dispose() 
+
+            int i = 0;
+
+            while (stream.EndOfStream == false) // fino a quando tutto il dato stream non è stato scansionato
+            {
+
+                var linea = stream.ReadLine(); //leggi una linea e restituisce una stringa
+
+                i++;
+
+                if (i <= 1) // Ignoro la prima riga del testo
+                    continue;
+
+                // Converto ogni linea in un indirizzo
+                try
+                {
+                    var dati = linea.Split(','); // Supponendo che il separatore sia dato dalla virgola
+
+                    //se la lunghezza della singola riga del file è minore di 6
+                    if (dati.Length < 6)
+                    {
+
+                        Console.WriteLine("\nStringa non valida: " + linea + "\n");
+                    }
+                    else
+                    {
+
+
+                        //acquisisco ogni dato dalle dovute posizioni
+                        string name = dati[0];
+                        string surname = dati[1];
+                        string street = dati[2];
+                        string city = dati[3];
+                        string province = dati[4];
+                        int zip = int.Parse(dati[5]);
+
+                        if (string.IsNullOrEmpty(street))
+                        {
+                            street = "Valore assente";
+                        }
+
+                        if (string.IsNullOrEmpty(name))
+                        {
+                            name = "Valore assente";
+                        }
+
+                        if (string.IsNullOrEmpty(province))
+                        {
+                            province = "Valore assente";
+                        }
+
+                        //creo un oggetto indirizzo passando le informazioni acquisite precedentemente
+                        Indirizzo adres = new Indirizzo(name, surname, street, city, province, zip);
+
+                        //inserisco all'interno della mia lista di indirizzi il nuovo indirizzo
+                        listaIndirizzi.Add(adres);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+
+
+
+            }
+
+            // Stampa degli indirizzi
+            foreach (var indirizzo in listaIndirizzi)
+            {
+                Console.WriteLine(indirizzo.ToString());
+
+
+            }
+            //chiamo funzione per la scrittura in un nuovo file
+            ScriviInTesto(listaIndirizzi, filePath2);
+        }
+
+        public static void ScriviInTesto(List<Indirizzo> listaIndirizzi, string filePath)
+        {
+            using StreamWriter stream = File.CreateText(filePath); // using può essere usato solo quando si hanno delle classi che implementano IDisposable
+            foreach (var indirizzo in listaIndirizzi)
+                stream.WriteLine(indirizzo.ToString());
+        }
+    }
+}
